5b0e55fdf3815134eafcce95f3ee42bd1d3a9540
Patrick Klie <info@patman-industries.com>
2019-07-08 01:10:41 +0200

    added first part of spline based creation of trajectory points

--- src/main.cpp
+++ src/main.cpp
@@ -51,10 +51,17 @@ int main() {
     map_waypoints_dy.push_back(d_y);
   }
 
+
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
                &map_waypoints_dx,&map_waypoints_dy]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
+                // start in ian
+    int lane = 1;
+
+    // Have a referene velocity to target
+    double ref_vel = 49.5;
+
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
@@ -87,9 +94,14 @@ int main() {
 
           // Sensor Fusion Data, a list of all other cars on the same side 
           //   of the road.
-          auto sensor_fusion = j[1]["sensor_fusion"];
+          auto sensor_fusion = j[1]["sensor_fusion"]; // this is vector<vector<double>>
 
-          json msgJson;
+          int prev_size = previous_path_x.size();
+
+          //if(prev_size > 0)
+          //{
+          //  car_s = end_path_s;
+          //}
 
           vector<double> next_x_vals;
           vector<double> next_y_vals;
@@ -99,20 +111,150 @@ int main() {
            *   sequentially every .02 seconds
            */
           // 1st test from the course: drive straight ahead
-          double dist_inc = 0.3;
-          for(int i=0; i<50; i++)
+          // 2nd test: follow waypoints in a simple manner
+          enum { ALGO_STRAIGHT = 0,
+                 ALGO_JUST_FOLLOW_WAYPOINTS = 1,
+                 ALGO_BETTER_FOLLOW_WAYPOINTS = 2
+          };
+          int l_algo = ALGO_BETTER_FOLLOW_WAYPOINTS;
+          if(l_algo == ALGO_BETTER_FOLLOW_WAYPOINTS)
           {
-            double next_s = car_s + (i+1)*dist_inc;
-            double next_d = 6.0;
-            vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+            vector<double> ptsx;
+            vector<double> ptsy;
 
-            //next_x_vals.push_back(car_x+(dist_inc*i)*cos(deg2rad(car_yaw)));
-            //next_y_vals.push_back(car_y+(dist_inc*i)*sin(deg2rad(car_yaw)));
-            next_x_vals.push_back(xy[0]);
-            next_y_vals.push_back(xy[1]);
+            // reference x,y, yaw states
+            // either reference starting point as where the car is or at the prev paths end point
+            double ref_x   = car_x;
+            double ref_y   = car_y;
+            double ref_yaw = deg2rad(car_yaw);
 
+            // if prev size is almost empty, use car as starting ref
+            if(prev_size < 2)
+            {
+              // use two points that make the path tangent to the car
+              double prev_car_x = car_x - cos(car_yaw);
+              double prev_car_y = car_y - sin(car_yaw);
+
+              ptsx.push_back(prev_car_x);
+              ptsx.push_back(car_x);
+
+              ptsy.push_back(prev_car_y);
+              ptsy.push_back(car_y);
+            }
+            // use prev path's end point as starting ref
+            else
+            {
+              // redefine ref state as previous path end point
+              ref_x = previous_path_x[prev_size-1];
+              ref_y = previous_path_y[prev_size-1];
+
+              double ref_x_prev = previous_path_x[prev_size-2];
+              double ref_y_prev = previous_path_y[prev_size-2];
+              ref_yaw = atan2(ref_y-ref_y_prev, ref_x-ref_x_prev);
+
+              // use 2 points that make path tangent to the previous path's end point
+              ptsx.push_back(ref_x_prev);
+              ptsx.push_back(ref_x);
+
+              ptsy.push_back(ref_y_prev);
+              ptsy.push_back(ref_y);
+
+            }
+            // In Frenet add evenly 30m spaced points ahead of the starting reference
+            vector<double> next_wp0 = getXY(car_s+30,(2+4*lane),map_waypoints_s,map_waypoints_x,map_waypoints_y);
+            vector<double> next_wp1 = getXY(car_s+60,(2+4*lane),map_waypoints_s,map_waypoints_x,map_waypoints_y);
+            vector<double> next_wp2 = getXY(car_s+90,(2+4*lane),map_waypoints_s,map_waypoints_x,map_waypoints_y);
+
+            ptsx.push_back(next_wp0[0]);
+            ptsx.push_back(next_wp1[0]);
+            ptsx.push_back(next_wp2[0]);
+
+            ptsy.push_back(next_wp0[1]);
+            ptsy.push_back(next_wp1[1]);
+            ptsy.push_back(next_wp2[1]);
+
+            for(int i=0; i<ptsx.size(); i++)
+            {
+              // shift car reference angle to 0 degrees
+              double shift_x = ptsx[i]-ref_x;
+              double shift_y = ptsy[i]-ref_y;
+
+              ptsx[i] = (shift_x * cos(0.0-ref_yaw)-shift_y*sin(0-ref_yaw));
+              ptsy[i] = (shift_x * sin(0.0-ref_yaw)-shift_y*cos(0-ref_yaw));
+            }
+
+            // create a spline
+            tk::spline s;
+
+            // set (x,y) points to the spline
+            s.set_points(ptsx, ptsy);
+
+            // start with all of the previous path points from last time
+            for(int i=0; i<previous_path_x.size(); i++)
+            {
+              next_x_vals.push_back(previous_path_x[i]);
+              next_y_vals.push_back(previous_path_y[i]);
+            }
+
+            // Calculate how to break up spline points
+            double target_x    = 30.0;
+            double target_y    = s(target_x);
+            double target_dist = sqrt((target_x)*(target_x)+(target_y)*(target_y));
+
+            double x_add_on = 0;
+
+            // fill up rest of path planner after filling it with prev points, here always output 50 points
+            for(int i=1; i<=50-previous_path_x.size(); i++)
+            {
+              double N = (target_dist/(.02*ref_vel)/2.24);  // convert from mph to m/s
+              double x_point = x_add_on+(target_x)/N;
+              double y_point = s(x_point);
+
+              x_add_on = x_point;
+
+              double x_ref = x_point;
+              double y_ref = y_point;
+
+              // rotate back to normal after rotating it earlier
+              x_point = (x_ref * cos(ref_yaw)-y_ref*sin(ref_yaw));
+              y_point = (x_ref * sin(ref_yaw)+y_ref*cos(ref_yaw));
+
+              x_point += ref_x;
+              y_point += ref_y;
+
+              next_x_vals.push_back(x_point);
+              next_y_vals.push_back(y_point);
+            }
           }
 
+          if(l_algo != ALGO_BETTER_FOLLOW_WAYPOINTS)
+          {
+            double dist_inc = 0.3;
+            for(int i=0; i<50; i++)
+            {
+              double next_s = car_s + (i+1)*dist_inc;
+              double next_d = 6.0;
+              vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+              if(l_algo == ALGO_STRAIGHT)
+              {
+                next_x_vals.push_back(car_x+(dist_inc*i)*cos(deg2rad(car_yaw)));
+                next_y_vals.push_back(car_y+(dist_inc*i)*sin(deg2rad(car_yaw)));
+              }
+              else if(l_algo == ALGO_JUST_FOLLOW_WAYPOINTS)
+              {
+                next_x_vals.push_back(xy[0]);
+                next_y_vals.push_back(xy[1]);
+              }
+              else if(l_algo == ALGO_BETTER_FOLLOW_WAYPOINTS)
+              {
+
+              }
+
+            }
+          }
+
+          json msgJson;
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
