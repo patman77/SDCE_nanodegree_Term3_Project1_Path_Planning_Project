62d9a24638777e313207562e05785067afd791b5
Patrick Klie <p@dunreeb.com>
2019-07-10 01:09:35 +0200

    * added collision avoidance

--- src/main.cpp
+++ src/main.cpp
@@ -98,11 +98,44 @@ int main() {
 
           int prev_size = previous_path_x.size();
 
-          //if(prev_size > 0)
-          //{
-          //  car_s = end_path_s;
-          //}
-
+          // lane change section
+          if(prev_size > 0)
+          {
+            car_s = end_path_s;
+          }
+          bool too_close = false;
+          
+          // find ref_v to use
+          for(int i=0; i<sensor_fusion.size(); i++)
+          {
+            // car is in my lane
+            float d = sensor_fusion[i][6];
+            if(d<(2+4*lane+2) && d>(2+4*lane-2))
+            {
+              double vx = sensor_fusion[i][3];
+              double vy = sensor_fusion[i][4];
+              double check_speed = sqrt(vx*vx+vy*vy);
+              double check_car_s = sensor_fusion[i][5];
+              
+              check_car_s += (static_cast<double>(prev_size)*0.02*check_speed);
+              // s values greater than mine and s gap
+              if((check_car_s > car_s) && ((check_car_s-car_s)<30))
+              {
+                // lower reference vel in order not to crash into the front car
+                //ref_vel = 29.5;
+                too_close = true;
+              }
+            }
+          }
+          
+          if(too_close)
+          {
+            ref_vel -= .224;
+          }
+          else if(ref_vel < 49.5)
+          {
+            ref_vel += .224;
+          }
 
           /**
            * TODO: define a path made up of (x,y) points that the car will visit
